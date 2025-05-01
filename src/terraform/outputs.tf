5b3d6020884362a36a8c7c407e63dfebcbbc3410
blee1993 <breannamarielee@gmail.com>
2025-05-01 19:14:57 -0400

    add redshift cluster

--- src/terraform/outputs.tf
+++ src/terraform/outputs.tf
@@ -12,3 +12,23 @@ output "lambda_function_arn" {
   description = "ARN of the Lambda function"
   value       = aws_lambda_function.ingestion_lambda.arn
 }
+
+output "redshift_cluster_identifier" {
+  description = "Identifier of the Redshift cluster"
+  value       = aws_redshift_cluster.redshift_cluster.cluster_identifier
+}
+
+output "redshift_cluster_endpoint" {
+  description = "Endpoint address of the Redshift cluster"
+  value       = aws_redshift_cluster.redshift_cluster.endpoint
+}
+
+output "redshift_database_name" {
+  description = "Database name of the Redshift cluster"
+  value       = aws_redshift_cluster.redshift_cluster.database_name
+}
+
+output "redshift_secret_arn" {
+  description = "ARN of the Secrets Manager secret storing Redshift connection info"
+  value       = aws_secretsmanager_secret.redshift_connection.arn
+}
